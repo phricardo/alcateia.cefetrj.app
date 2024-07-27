1e24c136a4dc43cbcacef2024183ad306acbd683
Pedro Ricardo <brpedroricardo@gmail.com>
2024-07-27 15:13:16 -0300

    feat: add draft card page

--- src/app/carteirinha/page.tsx
+++ src/app/carteirinha/page.tsx
@@ -3,11 +3,13 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import { UserContext } from "@/contexts/user-context";
-import styles from "./page.module.css";
+import { useQRCode } from "next-qrcode";
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function StudentIdCardPage() {
   const router = useRouter();
+  const { Canvas } = useQRCode();
   const { user, isLoading } = React.useContext(UserContext);
 
   React.useEffect(() => {
@@ -27,6 +29,13 @@ export default function StudentIdCardPage() {
         <div className={styles.card}>
           <div className={styles.cardHeader}>
             <Image
+              src={`/cefetrj.png`}
+              alt={`Foto de ${user?.name}`}
+              height={50}
+              width={201}
+            />
+
+            <Image
               src={`https://ui-avatars.com/api/?size=120&background=dddddd&color=000000&name=${user?.name}`}
               alt={`Foto de ${user?.name}`}
               height={150}
@@ -41,6 +50,20 @@ export default function StudentIdCardPage() {
             <li>CPF: {user.document?.id}</li>
             <li>Periodo de Mtricula: {user.enrollmentPeriod}</li>
           </ul>
+
+          <Canvas
+            text={user.studentId}
+            options={{
+              errorCorrectionLevel: "M",
+              margin: 0,
+              scale: 4,
+              width: 150,
+              color: {
+                dark: "#000000",
+                light: "#FFFFFF",
+              },
+            }}
+          />
         </div>
       </div>
     );
