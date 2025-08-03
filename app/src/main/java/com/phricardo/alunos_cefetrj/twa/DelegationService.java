d9c62be85b920d07bbbbf835d93ea526ccebbde9
Pedro Ricardo <brpedroricardo@gmail.com>
2025-08-03 02:30:48 -0300

    chore: update twa

---
+++ app/src/main/java/com/phricardo/alunos_cefetrj/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.phricardo.alunos_cefetrj.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
